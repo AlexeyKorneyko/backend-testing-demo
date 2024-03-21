782d6d79fa07606018b9c3e8e30ef5489476fa7d
Alexey Korneyko <alexey@finmid.com>
2024-03-21 16:54:53 +0100

    rename files to reflect frameworks

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,12 +9,6 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testApi("org.assertj:assertj-core:3.25.3")
-    testApi("io.kotest:kotest-assertions-core:5.8.0")
-}
-
 tasks.test {
     useJUnitPlatform()
 }
